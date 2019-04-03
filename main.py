fc839fb3fd625c66e25db5e9460b5dced40138ac
agentOfChaos <informaniac99@gmail.com>
2019-04-03 19:25:55 +0200

    handled ReactorAlreadyInstalledError exception

--- main.py
+++ main.py
@@ -3,12 +3,19 @@ from PySide2.QtUiTools import QUiLoader
 from PySide2.QtWidgets import QApplication
 from PySide2.QtCore import QFile
 
+from twisted.internet.error import ReactorAlreadyInstalledError
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
 
     import qt5reactor
-    qt5reactor.install()
+
+    try:
+        qt5reactor.install()
+    except ReactorAlreadyInstalledError:
+        pass
+
     from twisted.internet import reactor
 
     file = QFile("mainwindow.ui")
